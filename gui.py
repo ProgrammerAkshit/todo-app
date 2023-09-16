46936a8b35e4b2018140100b0f3ba3dae3c86a06
akshi <akshit08sharma@gmail.com>
2023-09-16 16:42:27 +0530

    Implemended Add button

--- gui.py
+++ gui.py
@@ -2,8 +2,23 @@ import function
 import PySimpleGUI as sg
 
 label = sg.Text("Type Your To-Do")
-input_box = sg.InputText(tooltip="Enter Your To-Do")
+input_box = sg.InputText(tooltip="Enter Your To-Do", key="todo")
 add_button = sg.Button("Add")
-window = sg.Window('''Akshit's To-Do App''', layout=[[label], [input_box, add_button]])
-window.read()
+window = sg.Window('''Akshit's To-Do App''',
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica' , 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = function.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            function.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
